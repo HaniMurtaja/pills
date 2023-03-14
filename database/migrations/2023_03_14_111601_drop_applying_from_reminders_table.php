9f304bd31e66dc5de9aee9af64e7cf8261041e16
Hani Murtaja <hanimurtaja@Hanis-MacBook-Pro.local>
2023-03-14 20:02:19 +0200

    reminderstable

--- database/migrations/2023_03_14_111601_drop_applying_from_reminders_table.php
+++ database/migrations/2023_03_14_111601_drop_applying_from_reminders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('reminders', function (Blueprint $table) {
-            $table->string('applying')->nullable();
+            $table->string('applying');
         });
     }
 
