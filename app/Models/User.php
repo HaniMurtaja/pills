7cade36888db0f22581374e8529ca78129bb9e9c
“Hani <hanifalah047@gmail.com>
2023-03-28 01:40:53 +0300

    resetformat

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use \DateTimeInterface;
+use \DateTimeInterface;
 use App\Notifications\VerifyUserNotification;
 use Carbon\Carbon;
 use Hash;
@@ -225,9 +225,8 @@ class User extends Authenticatable implements HasMedia
         return $this->belongsToMany(UserHealth::class);
     }
 
-  /*  protected function serializeDate(DateTimeInterface $date)
+    protected function serializeDate(DateTimeInterface $date)
     {
         return $date->format('Y-m-d H:i:s');
     }
-    */
 }
